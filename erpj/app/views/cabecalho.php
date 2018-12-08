0088e1bbb2608c8e57d51537dc7fb9bca8e1706a
conejofoz <silviocoelho.udc@gmail.com>
2018-12-08 07:02:55 -0200

    FATIANDO O LAYOUT

--- erpj/app/views/cabecalho.php
+++ erpj/app/views/cabecalho.php
@@ -1,8 +1,19 @@
-<?php
+<div class="base-topo relative">
+    <div class="conteudo">
+        <h1 class="logo">			
+            <span class="beta"></span>
+            <span class="icone menucat menu-anchor">Menu</span>		
+            <a href="https://mjailton.com.br/testes/erp_oficial/versao02/"> ERP mjailton</a>						
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        </h1>
+        <ul class="menu-topo">
+            <li><a href="" class="icomin"><i class="ico-msg">mensagem</i><span class="msg">3</span>	</a></li>
+            <li><a href="" class="icomin"><i class="ico-aviso">aviso</i> <span class="aviso">2</span>	</a></li>
 
+            <li><a href="">LOGIN</a></li>
+            <li><a href="">CADASTRAR</a></li>
+
+        </ul>
+    </div>
+
+</div>		
--- erpj/app/views/menu.php
+++ erpj/app/views/menu.php
@@ -1,8 +1,121 @@
-<?php
+        <div class="menu-lateral">
+            <menu>	
+                <dl>
+                    <ul class="sessao base_user">
+                        <a href="<?php echo URL_BASE ?>">
+                            <div class="cel3">
+                                <div class="thumb"><img src="<?php echo URL_BASE ?>assets/img/foto01.png"></div>
+                            </div>
+                            <div class="cel9">
+                                <span>Jailton Sousa nascimento</span>
+                                <small>Admin</small>
+                                <small class="online">online</small>
+                            </div>
+                        </a>
+                    </ul>
+                    <ul class="sessao">
+                        <li><a href="index.php?link=1"><i class="icones home"></i>  home</a></li>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                    </ul>
+                    <ul class="sessao">
+                        <h2>Módulo 01: Cadastros</h2>
+                        <h3>Cadastros</h3>
+
+                        <li class="sub2">
+                        <dt><a href=""><i class="icones pedido"></i>Produto	</a></dt>
+                        <dd>
+                            <ul>
+                                <li><a href="<?php echo URL_BASE . "produto" ?>">Produto</a></li>
+                                <li><a href="<?php echo URL_BASE . "categoria" ?>">Categoria</a></li>
+                                <li><a href="<?php echo URL_BASE . "subcategoria" ?>">Subcategoria</a></li>
+                                <li><a href="<?php echo URL_BASE . "grupo" ?>">Grupo</a></li>
+                                <li><a href="<?php echo URL_BASE . "subgrupo" ?>">SubGrupo</a></li>
+                                <li><a href="<?php echo URL_BASE . "marca" ?>">Marca</a></li>
+                                <li><a href="<?php echo URL_BASE . "modelo" ?>">Modelo</a></li>
+                            </ul>
+                        </dd>
+                        </li>			
+                        <li><a href="lst_cliente.html"><i class="icones pedido"></i>Cliente	</a></li>	
+                    </ul>
+                    <ul class="sessao">
+                        <h2>Módulo 02: Vendas</h2>
+                        <h3>PDV</h3>
+                        <li><a href="lst_pdv.html"><i class="icones saida"></i>Vendas	</a></li>		
+
+                        <h3>Pedidos</h3>		
+                        <li><a href="lst_todos_pedido.html"></i><i class="icones pedido"></i>Todos os Pedidos	</a></li>			
+                        <li><a href="lst_pedido_pendente.html"><i class="icones pedido"></i>Pedidos Pendentes	</a></li>			
+                        <li><a href="novo_pedido.html"><i class="icones pedido"></i>Novo Pedido	</a></li>	
+
+
+                        <h3>Loja Virtual</h3>
+                        <li><a href="lst_venda_loja.html"><i class="icones saida"></i>Todas as Vendas	</a></li>			
+                        <li><a href="lst_nao_enviadas.html"><i class="icones entrada"></i>Vendas Não Enviadas </a></li>
+                        <li><a href="lst_nao_pagas.html"><i class="icones entrada"></i>Vendas Não Pagas </a></li>		
+                        <li><a href="lst_vendas_pagas.html"><i class="icones fornecedor"></i>Vendas Pagas</a></li>
+
+                        <h3>Movimentações em Geral</h3>
+                        <li><a href="lst_movimentacao.html"><i class="icones saida"></i>Todas as Movimentações	</a></li>			
+                        <li><a href="lst_resulmo_vendas.html"><i class="icones entrada"></i>Resumo de Vendas </a></li>
+                    </ul>
+
+                    <ul class="sessao">
+                        <h2>Módulo 03: NFE</h2>
+                        <h3>Nota Fiscal Eletrônica </h3>
+                        <li><a href="lst_notas.html"><i class="icones nfe"></i>Lista de Notas</a></li>		
+                    </ul>
+
+                    <ul class="sessao">
+                        <h2>Módulo 04: Estoque</h2>
+                        <h3>Estoque</h3>
+                        <li><a href="lst_entrada.html"><i class="icones saida"></i>Entrada</a></li>
+                        <li><a href="lst_vendas.html"><i class="icones saida"></i>Vendas	</a></li>		
+                    </ul>
+
+                    <ul class="sessao">
+                        <h2>Módulo 05: Financeiro</h2>
+                        <h3>Entradas</h3>		
+                        <li><a href=""><i class="icones entrada"></i>entrada</a></li>
+                        <li><a href=""><i class="icones fornecedor"></i>fornecedor</a></li>
+                    </ul>
+                    <ul class="sessao">
+                        <h3>Saídas</h3>
+                        <li><a href=""><i class="icones saida"></i>saída	</a></li>	
+                        <li><a href=""><i class="icones fornecedor"></i>fornecedor</a></li>
+                    </ul>	
+
+                    <ul class="sessao">
+                        <h2>Módulo 06: Pessoal</h2>
+                        <h3>DEPARTAMENTO PESSOAL</h3>
+                        <li><a href=""><i class="icones demissoes"></i>DEMISSÕES	</a></li>			
+                        <li><a href=""><i class="icones admissoes"></i>ADMISSÕES	</a></li>			
+                        <li><a href=""><i class="icones f-pagamento"></i>FOLHA DE PAGAMENTO</a></li>
+                        <li><a href=""><i class="icones cont-ponto"></i>CONTROLE DE PONTO</a></li>
+                    </ul>
+
+                    <ul class="sessao">
+                        <h2>Módulo 07: Contábil/Fiscal</h2>
+                        <h3>FISCAL</h3>
+                        <li><a href=""><i class="icones entrada-saida"></i>ENTRADAS E SAÍDAS	</a></li>			
+                        <li><a href=""><i class="icones apuracao"></i>APURAÇÃO E TRIBUTOS	</a></li>			
+                        <li><a href=""><i class="icones receita"></i>RECEITAS</a></li>
+                        <li><a href=""><i class="icones despesas"></i>DESPESAS</a></li>
+                        <li><a href=""><i class="icones custos"></i>CUSTOS</a></li>
+                    </ul>	
+
+                    <ul class="sessao">
+                        <h3>CONTÁBIL</h3>
+                        <li><a href=""><i class="icones demo-contabel"></i>DEMONSTRAÇÕES CONTÁBEIS</a></li>			
+                        <li><a href=""><i class="icones lancamento-debito"></i>LANÇAMENTO DE CRÉDITO E DÉBITO</a></li>	
+                    </ul>
+                    <ul class="sessao">
+                        <h2>Módulo 08: Produção</h2>
+                        <h3>PRODUÇÃO</h3>
+                        <li><a href="#"><i class="icones materia"></i>MATÉRIA-PRIMA</a></li>
+                        <li><a href="#"><i class="icones produto"></i>CUSTO DE PRODUÇÃO</a></li>				
+                        <li><a href="#"><i class="icones estoque"></i>ESTOQUE MATÉRIA-PRIMA</a></li>		
+                    </ul>		
+                </dl>		
+            </menu>
+        </div>
 
